fa7b3eca823239e6705e0a6a471de54a936dd5b1
Anthony <75976834+anthony-mephon@users.noreply.github.com>
2025-11-09 22:45:41 +0000

    Version finalise qui prends en compte les espaces, les nombres et les caracteres speciaux

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+"""Module pour vérifier si une chaîne est un palindrome."""
+
 #### Fonction secondaire
+ACCENTS = "ÀÁÂÃÄÅàáâãäåÈÉÊËèéêëÌÍÎÏìíîïÒÓÔÕÖØòóôõöøÙÚÛÜùúûüÝýÿÇç"
+SANS_ACCENTS = "AAAAAAaaaaaaEEEEeeeeIIIIiiiiOOOOOOooooooUUUUuuuuYyyCc"
+table = str.maketrans(ACCENTS, SANS_ACCENTS)
 
 
 def ispalindrome(p):
-
+    """Vérifie si la chaîne p est un palindrome."""
     # votre code ici
-    
-    return False
+    p = p.translate(table)
+    p = p.lower()
+    p = ''.join(c for c in p if c.isalnum())
+    print(p)
+    return p == p[::-1]
 
 #### Fonction principale
 
 
 def main():
-
+    """Fonction principale pour tester la fonction ispalindrome."""
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
